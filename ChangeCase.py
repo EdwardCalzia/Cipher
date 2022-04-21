2b719ceaa6ebe1541ef8bf4ecab7d9d9282bd7fd
Edward Calzia <84807135+EdwardCalzia@users.noreply.github.com>
2022-04-21 08:53:08 +0100

    Update ChangeCase.py

--- ChangeCase.py
+++ ChangeCase.py
@@ -1,1 +1,14 @@
+string = str(input("Enter a text: "))
+newstring ='' 
 
+for a in string: 
+    if (a.isupper()) == True: 
+        newstring+=(a.lower()) 
+          
+    elif (a.islower()) == True: 
+        newstring+=(a.upper())
+          
+    elif (a.isspace()) == True:
+        a.replace(" ", "")
+
+print(newstring)
